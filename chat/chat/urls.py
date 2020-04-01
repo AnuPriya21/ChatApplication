f93a5a46a270b989606d1e2375a2e99f271c1028
AnuPriya21 <anupriyam2105@gmail.com>
2020-04-01 14:41:08 +0530

    [Anu Priya] Refactor. Refactored url for chatview

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('registration/', Registrations.as_view(), name="registration"),
     path('chatapp/',views.chatapp,name = 'chatapp'),
 
-    path('chat/',chat_view, name='chats'),
+    path('chat/',chat_view, name='chat'),
     path('chat/<int:sender>/<int:receiver>/', message_view, name='chat'),
     path('api/messages/<int:sender>/<int:receiver>/', message_list, name='message-detail'),  
     path('api/messages/', message_list, name='message-list'),   
